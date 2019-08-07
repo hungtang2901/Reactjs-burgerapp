aac28d4ebf673ade6ed8d48e33d15b838ff39129
Hung Tang <hung.tang@sjsu.edu>
2019-08-06 19:20:15 -0700

    created burger app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This is Burger App</p>
+    </div>
+  );
+}
+
+export default App;

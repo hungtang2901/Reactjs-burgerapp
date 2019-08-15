d2240e91423a19b86d774a1540b79a56e47c26f5
Hung Tang <hung.tang@sjsu.edu>
2019-08-15 10:12:53 -0700

    Adding drawer toggle

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem'
 const navigationItems = () => (
    <ul className={classes.NavigationItems}>
        <NavigationItem link='/' active>Burger Builder</NavigationItem>
-       <NavigationItem link='/'>Checkout</NavigationItem>
+       <NavigationItem link='/'active>Checkout</NavigationItem>
        
    </ul>
 )
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,15 +2,21 @@ import React from 'react';
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+//import MenuLogo from '../../../assets/images/baseline_menu_black_18dp.png'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
         <div>
-            MENU
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
+            
         </div>
+        <div className={classes.Logo}>
         <Logo />
-        <nav>
+        </div>
+        
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
